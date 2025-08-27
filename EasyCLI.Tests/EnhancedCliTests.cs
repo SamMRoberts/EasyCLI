b5d8f3546ea09d0b0bb3e012322dfdc68bea48e6
Sam Roberts <sam.m.roberts@icloud.com>
2025-08-27 12:51:24 -0400

    Refactor EnhancedCliTests: reorganize using directives and improve code formatting

--- EasyCLI.Tests/EnhancedCliTests.cs
+++ EasyCLI.Tests/EnhancedCliTests.cs
@@ -1,47 +1,51 @@
-namespace EasyCLI.Tests;
+using EasyCLI.Console;
+using EasyCLI.Shell;
 
-public class EnhancedCliTests
+namespace EasyCLI.Tests
 {
-    [Fact]
-    public void CommandLineArgs_ParsesFlags_Correctly()
+    public class EnhancedCliTests
     {
-        var args = new CommandLineArgs(new[] { "--help", "--verbose", "-q" });
+        [Fact]
+        public void CommandLineArgs_ParsesFlags_Correctly()
+        {
+            var args = new CommandLineArgs(new[] { "--help", "--verbose", "-q" });
 
-        Assert.True(args.IsHelpRequested);
-        Assert.True(args.IsVerbose);
-        Assert.True(args.IsQuiet);
-        Assert.False(args.IsDryRun);
-    }
+            Assert.True(args.IsHelpRequested);
+            Assert.True(args.IsVerbose);
+            Assert.True(args.IsQuiet);
+            Assert.False(args.IsDryRun);
+        }
 
-    [Fact]
-    public void CommandLineArgs_ParsesOptions_Correctly()
-    {
-        var args = new CommandLineArgs(new[] { "--output", "file.txt", "--repeat=3", "-r", "5" });
+        [Fact]
+        public void CommandLineArgs_ParsesOptions_Correctly()
+        {
+            var args = new CommandLineArgs(new[] { "--output", "file.txt", "--repeat=3", "-r", "5" });
 
-        Assert.Equal("file.txt", args.GetOption("output"));
-        Assert.Equal("3", args.GetOption("repeat"));
-        Assert.Equal("5", args.GetOption("r"));
-    }
+            Assert.Equal("file.txt", args.GetOption("output"));
+            Assert.Equal("3", args.GetOption("repeat"));
+            Assert.Equal("5", args.GetOption("r"));
+        }
 
-    [Fact]
-    public void CommandLineArgs_ParsesPositionalArguments_Correctly()
-    {
-        var args = new CommandLineArgs(new[] { "command", "arg1", "arg2", "--flag" });
+        [Fact]
+        public void CommandLineArgs_ParsesPositionalArguments_Correctly()
+        {
+            var args = new CommandLineArgs(new[] { "command", "arg1", "arg2", "--flag" });
 
-        Assert.Equal(3, args.Arguments.Count);
-        Assert.Equal("command", args.GetArgument(0));
-        Assert.Equal("arg1", args.GetArgument(1));
-        Assert.Equal("arg2", args.GetArgument(2));
-        Assert.True(args.HasFlag("flag"));
-    }
+            Assert.Equal(3, args.Arguments.Count);
+            Assert.Equal("command", args.GetArgument(0));
+            Assert.Equal("arg1", args.GetArgument(1));
+            Assert.Equal("arg2", args.GetArgument(2));
+            Assert.True(args.HasFlag("flag"));
+        }
 
-    [Fact]
-    public void ExitCodes_HasStandardValues()
-    {
-        Assert.Equal(0, ExitCodes.Success);
-        Assert.Equal(1, ExitCodes.GeneralError);
-        Assert.Equal(2, ExitCodes.FileNotFound);
-        Assert.Equal(4, ExitCodes.InvalidArguments);
-        Assert.Equal(127, ExitCodes.CommandNotFound);
+        [Fact]
+        public void ExitCodes_HasStandardValues()
+        {
+            Assert.Equal(0, ExitCodes.Success);
+            Assert.Equal(1, ExitCodes.GeneralError);
+            Assert.Equal(2, ExitCodes.FileNotFound);
+            Assert.Equal(4, ExitCodes.InvalidArguments);
+            Assert.Equal(127, ExitCodes.CommandNotFound);
+        }
     }
 }
